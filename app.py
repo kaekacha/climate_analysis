6627b541ca36c30d78721ed3b9331b3cd50981d1
kaekacha <56618353+kaekacha@users.noreply.github.com>
2021-03-07 19:11:47 -0500

    finalized codes

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+#import dependencies
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect, desc 
 from flask import Flask, jsonify
+from datetime import datetime
+from dateutil.relativedelta import *
+
+#create orms from sqlite database tables "measurement" and "station"
 database_path = "Resources/hawaii.sqlite"
 engine = create_engine(f"sqlite:///{database_path}")
 Base = automap_base()
@@ -13,6 +18,7 @@ station = Base.classes.station
 #boiler plate code: Create an app, being sure to pass __name__
 app = Flask(__name__)
 
+#created index/home route
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
@@ -22,9 +28,10 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start> and /api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/start and /api/v1.0/start/end Note: Please manually enter dates in YYYY-MM-DD formate for start, end."
     )
 
+#create precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     print("Server received request for 'precipitation' page...")
@@ -44,6 +51,7 @@ def precipitation():
 
     session.close()
 
+#create station route
 @app.route("/api/v1.0/stations")
 def stations():
     print("Server received request for 'stations' page...")
@@ -62,44 +70,100 @@ def stations():
 
     session.close()
 
+#create tobs route
 @app.route("/api/v1.0/tobs")
 def tobs():
     print("Server received request for 'tobs' page...")
 
     session = Session(engine)
     
-    to_hist=session.query(measurement.date, measurement.tobs).filter(measurement.station==temps[0]).order_by(desc(measurement.date)).first()
-    
-    tobs_list= []
-    for station, tobs in st_sesh:
-        st_dict = {}
-        st_dict["station"] = st
-        st_list.append(st_dict)
+    st_active=session.query(measurement.station,func.count(measurement.station)).\
+        group_by(measurement.station).\
+        order_by(desc(func.count(measurement.station))).first()
+   
+    tobs_active =session.query(measurement.date, measurement.tobs).\
+        filter(measurement.station==st_active[0]).\
+        order_by(desc(measurement.date)).first()
 
-    return jsonify(st_list)
+    recent_dt=tobs_active[0]
+    
+    import datetime
+    from dateutil.relativedelta import relativedelta
+
+    recent_dt_form = datetime.datetime.strptime(recent_dt, '%Y-%m-%d')
+    recent_dt=recent_dt_form.date()
+    past_dt=recent_dt+relativedelta(years=-1)
+    tobs_pastyr_active=session.query(measurement.date, measurement.tobs).\
+        filter(measurement.station==st_active[0]).filter(measurement.date>=past_dt).all()
+
+    tobs_list= []
+    for date, tobs in tobs_pastyr_active:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_list.append(tobs_dict)
+
+    return jsonify(tobs_list)
 
     session.close()
-'''
-/api/v1.0/tobs
 
-Query the dates and temperature observations of the most active station for the last year of data.
+#create manual start date entry route
+@app.route("/api/v1.0/<start>")
+def start(start):
+    print("Server received request for 'start date' page...")
+   
+    session = Session(engine)
 
-Return a JSON list of temperature observations (TOBS) for the previous year.
+    import datetime
+    from dateutil.relativedelta import relativedelta
 
-/api/v1.0/<start> and /api/v1.0/<start>/<end>
+    start_dt = datetime.datetime.strptime(start, '%Y-%m-%d')
+    start_dt=start_dt.date()
+    tobs_start=session.query(measurement.date,func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+        filter(measurement.date>=start_dt).group_by(measurement.date).all()
 
-Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+    tobs_list= []
+    for date, min, avg, max in tobs_start:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["min"] = min
+        tobs_dict["avg"] = avg
+        tobs_dict["max"] = max
+        tobs_list.append(tobs_dict)
 
-When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+    return jsonify(tobs_list)
 
-When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    session.close()
 
-Hints
+#create manual start date entry route
+@app.route("/api/v1.0/<start>/<end>")
+def startend(start,end):
+    print("Server received request for 'start and end dates' page...")
 
-You will need to join the station and measurement tables for some of the queries.
+    session = Session(engine)
 
-Use Flask jsonify to convert your API data into a valid JSON response object.
-'''
+    import datetime
+    from dateutil.relativedelta import relativedelta
+
+    start_dt = datetime.datetime.strptime(start, '%Y-%m-%d')
+    start_dt=start_dt.date()
+    end_dt = datetime.datetime.strptime(end, '%Y-%m-%d')
+    end_dt=end_dt.date()
+    tobs_startend=session.query(measurement.date,func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+        filter(measurement.date>=start_dt).filter(measurement.date<=end_dt).group_by(measurement.date).all()
+
+    tobs_list= []
+    for date, min, avg, max in tobs_startend:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["min"] = min
+        tobs_dict["avg"] = avg
+        tobs_dict["max"] = max
+        tobs_list.append(tobs_dict)
+
+    return jsonify(tobs_list)
+
+    session.close()
 
 #boiler plate code
 if __name__ == "__main__":
